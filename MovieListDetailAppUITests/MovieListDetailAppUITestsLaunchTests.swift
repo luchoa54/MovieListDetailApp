2ff42bacfa6cef2c58c100813e804d12b4cf5025
lucianoUchoa <luciano.uchoa@icomp.ufam.edu.br>
2025-09-23 13:33:45 -0400

    Initial Commit

---
+++ MovieListDetailAppUITests/MovieListDetailAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MovieListDetailAppUITestsLaunchTests.swift
+//  MovieListDetailAppUITests
+//
+//  Created by Luciano Uchoa on 23/09/25.
+//
+
+import XCTest
+
+final class MovieListDetailAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
